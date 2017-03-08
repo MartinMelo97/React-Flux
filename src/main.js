ed1f962a14c677b5f993326a8c128424d74b606c
MartinMelo97 <nitram-210397@hotmail.com>
2017-03-08 12:59:07 -0600

    React

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 $ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homepage');
+var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
+var Authors = require('./components/authors/authorPage');
 
+var App = React.createClass({
+    render: function(){
+        var Child;
+        switch(this.props.route){
 
-var App = console.log("Hola mundo");
+            case 'about': Child = About; break;
+            case 'authors': Child = Authors; break;
+            default: Child = Home;
+        }
+        return (
+            <div>
+                <Header />
+                <Child />
+            </div>
+        );
+    }
+});
 
-module.exports = App;
+function render(){
+    var route = window.location.hash.substr(1);
+    React.render(<App route={route}/>, document.getElementById('app'));
+}
+
+window.addEventListener('hashchange', render);
+render();
+//React.render(<Home />, document.getElementById('app'));
